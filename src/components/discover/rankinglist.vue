c55bd89f0f9f9805c0c98e2269266bfe52e6b70e
xuzhongwei <943935598@qq.com>
2020-06-22 20:12:41 +0800

    排行榜详情

--- src/components/discover/rankinglist.vue
+++ src/components/discover/rankinglist.vue
@@ -9,11 +9,11 @@
     <div class="official">
       <div class="list-single" v-for="(item) in officiallist" :key="item.playCount" :data-name='item.name' @click.stop="toDetails($event)" ref="single">
         <div class="list-single-left">
-          <img :src="item.coverImgUrl" :alt="item.name">
-          <span>{{item.updateFrequency}}</span>
+          <img :src="item.coverImgUrl" :alt="item.name" :data-name='item.name'>
+          <span :data-name='item.name'>{{item.updateFrequency}}</span>
         </div>
         <div class="list-single-right">
-          <div v-for="(track, index) in item.tracks" :key="index">{{index+1}}. {{track.first}} - {{track.second}}</div>
+          <div v-for="(track, index) in item.tracks" :key="index" :data-name='item.name'>{{index+1}}. {{track.first}} - {{track.second}}</div>
         </div>
       </div>
     </div>
@@ -23,8 +23,8 @@
     <div class="other">
       <div class="list-single" v-for="(item) in otherList" :key="item.playCount" :data-name='item.name' @click.stop="toDetails($event)" ref="single">
         <div class="list-single-left">
-          <img :src="item.coverImgUrl" :alt="item.name">
-          <span>{{item.updateFrequency}}</span>
+          <img :src="item.coverImgUrl" :alt="item.name" :data-name='item.name'>
+          <span :data-name='item.name'>{{item.updateFrequency}}</span>
         </div>
       </div>
     </div>
@@ -46,7 +46,43 @@ export default {
         path: 'discover'
       },
       officiallist: [], // 官方榜
-      otherList: []
+      otherList: [], // 其他榜单
+      rankingIndex: {
+        云音乐新歌榜: 0,
+        云音乐热歌榜: 1,
+        网易原创歌曲榜: 2,
+        云音乐飙升榜: 3,
+        云音乐电音榜: 4,
+        UK排行榜周榜: 5,
+        美国Billboard周榜: 6,
+        KTV嗨榜: 7,
+        iTunes榜: 8,
+        'Hit FM Top榜': 9,
+        日本Oricon周榜: 10,
+        韩国Melon排行榜周榜: 11,
+        韩国Mnet排行榜周榜: 12,
+        韩国Melon原声周榜: 13,
+        '中国TOP排行榜(港台榜)': 14,
+        '中国TOP排行榜(内地榜)': 15,
+        香港电台中文歌曲龙虎榜: 16,
+        华语金曲榜: 17,
+        中国嘻哈榜: 18,
+        '法国 NRJ EuroHot 30周榜': 19,
+        台湾Hito排行榜: 20,
+        Beatport全球电子舞曲榜: 21,
+        云音乐ACG音乐榜: 22,
+        云音乐说唱榜: 23,
+        云音乐古典音乐榜: 24,
+        抖音排行榜: 26,
+        新声榜: 27,
+        云音乐韩语榜: 28,
+        英国Q杂志中文版周榜: 29,
+        电竞音乐榜: 30,
+        云音乐欧美热歌榜: 31,
+        云音乐欧美新歌榜: 32,
+        说唱TOP榜: 33
+      },
+      selectedIndex: ''
     }
   },
   computed: {
@@ -75,10 +111,15 @@ export default {
     toDetails (e) {
       const evn = e || window.event
       const target = evn.target
-      console.log(target.nodeName)
-      if (target.nodeName === 'IMG' || target.nodeName === 'DIV') {
-        this.$router.push({ name: 'rankinglist_details' })
-      }
+      const rankingType = target.getAttribute('data-name')
+      Object.keys(this.rankingIndex).forEach(ele => {
+        if (rankingType === ele) {
+          this.selectedIndex = this.rankingIndex[ele].toString()
+          sessionStorage.setItem('rankingListIndex', this.selectedIndex)
+          sessionStorage.setItem('rankingType', rankingType)
+          this.$router.push({ name: 'rankinglist_details' })
+        }
+      })
     }
   },
   components: {
--- src/components/discover/rankinglist_details.vue
+++ src/components/discover/rankinglist_details.vue
@@ -1,25 +1,39 @@
 // 排行榜点击进来详情
 <template>
-  <div>
-    Details
+  <div class="rankinglist-detail">
+    <goBack :showGoBack='showGoBack'/>
+    <div class="songlist">
+      <div class="single" v-for='item in tracks' :key="item.id">
+        <div class="info">
+          <div class="name">{{item.name}}</div>
+          <div class="singer"><span v-for="singer in item.ar" :key="singer.id">{{singer.name}}</span> - {{item.al.name}}</div>
+        </div>
+        <div class="play"><i class="fa fa-play-circle-o"></i></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import goBack from '@components/public/goBack.vue'
 export default {
   props: {
 
   },
   data () {
     return {
-
+      showGoBack: {
+        show: 1,
+        path: 'discover/rankinglist'
+      },
+      tracks: []
     }
   },
   computed: {
 
   },
   created () {
-
+    this.getPlayList()
   },
   mounted () {
 
@@ -28,14 +42,54 @@ export default {
 
   },
   methods: {
-
+    getPlayList () {
+      this.showGoBack.title = sessionStorage.getItem('rankingType')
+      const selectedIndex = sessionStorage.getItem('rankingListIndex')
+      this.$axios.get(`/top/list?idx=${selectedIndex}`).then(res => {
+        if (res.code === 200) {
+          this.tracks = res.playlist.tracks
+          console.log(this.tracks)
+        }
+      })
+    }
   },
   components: {
-
+    goBack
   }
 }
 </script>
 
 <style scoped lang="scss">
-
+.rankinglist-detail{
+  padding: 5%;
+  height: 100%;
+}
+.songlist{
+  .single{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 20px;
+    box-shadow: 10px 10px 10px rgba(0,0,0,.3);
+    padding: 10px;
+    box-sizing: border-box;
+    background-color: #BA5370;
+    border-radius: 4px;
+    .info{
+      width: 50%;
+      color: #fff;
+      .name{
+        font-weight: 600;
+      }
+      .singer{
+        font-size: 12px;
+        margin-top: 5px;
+      }
+    }
+    .play{
+      width: 4%;
+      color: #d3d3d3;
+    }
+  }
+}
 </style>
