d64af498e7b08cbebeab679a91481134152d9c2b
徐忠炜 <943935598@qq.com>
2020-06-24 14:45:34 +0800

    重写路由push方法

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+/**
+ * 重写路由的push方法
+ */
+const routerPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push (location) {
+  return routerPush.call(this, location).catch(error => error)
+}
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -35,7 +43,14 @@ const routes = [
   {
     path: '/search', // 搜索
     name: 'search',
-    component: () => import('@components/search/search.vue')
+    component: () => import('@components/search/search.vue'),
+    children: [
+      {
+        path: '/search/result',
+        name: 'result',
+        component: () => import('@/components/search/searchResult.vue')
+      }
+    ]
   },
   {
     path: '/discover/rankinglist', // 发现 - 排行榜
