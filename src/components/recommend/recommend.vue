d9a110b25a2b1111d96ba093f48d6eab4df16975
徐忠炜 <943935598@qq.com>
2020-06-30 09:55:21 +0800

    fix：接口更新 更改变量名

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -3,14 +3,14 @@
   <div class="recommend">
     <goBack :showGoBack='showGoBack'/>
     <div class="recommend-list">
-      <div class="recommend-list-single" v-for="(item, i) in recommendList" :key="item.id" :data-songId='item.privilege.id' :data-index='i' @click="play($event)">
+      <div class="recommend-list-single" v-for="(item, i) in recommendList" :key="item.id" :data-songId='item.id' :data-index='i' @click="play($event)">
         <div class="img">
-          <img :src="item.album.blurPicUrl" alt="" width="50" height="50">
+          <img :src="item.al.picUrl" alt="" width="50" height="50">
         </div>
         <div class="song-detail">
           <div class="name">{{item.name}}</div>
-          <div class="singer" v-if="item.artists.length === 1">{{item.artists[0].name}} - {{ item.album.name}}</div>
-          <div class="singer" v-else><span v-for="singer in item.artists" :key="singer.id">{{singer.name}} </span> - {{ item.album.name}}</div>
+          <div class="singer" v-if="item.ar.length === 1">{{item.ar[0].name}} - {{ item.al.name}}</div>
+          <div class="singer" v-else><span v-for="singer in item.ar" :key="singer.id">{{singer.name}} </span> - {{ item.al.name}}</div>
         </div>
         <div class="play"><i class="fa fa-play-circle-o"></i></div>
       </div>
@@ -40,7 +40,7 @@ export default {
   created () {
     this.$axios.get('/recommend/songs').then(res => {
       if (res.code === 200) {
-        this.recommendList = res.recommend
+        this.recommendList = res.data.dailySongs
       }
     })
   },
@@ -66,14 +66,14 @@ export default {
             const index = target.getAttribute('data-index')
             const playingSongInfo = {}
             let names = ''
-            if (this.recommendList[index].artists.length === 1) {
-              playingSongInfo.singer = this.recommendList[index].artists[0].name
+            if (this.recommendList[index].ar.length === 1) {
+              playingSongInfo.singer = this.recommendList[index].ar[0].name
             } else {
-              this.recommendList[index].artists.forEach(ele => { names += `${ele.name} ` })
+              this.recommendList[index].ar.forEach(ele => { names += `${ele.name} ` })
               playingSongInfo.singer = names
             }
             playingSongInfo.id = this.recommendList[index].id
-            playingSongInfo.blurPicUrl = this.recommendList[index].album.blurPicUrl
+            playingSongInfo.blurPicUrl = this.recommendList[index].al.picUrl
             playingSongInfo.name = this.recommendList[index].name
 
             localStorage.setItem('playingSong', JSON.stringify(playingSongInfo))
