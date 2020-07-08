ce7560c57cb1cbfa39d0096ad8c12e27fccf7e0d
徐忠炜 <943935598@qq.com>
2020-07-08 18:18:16 +0800

    fix：elementUI按需引入

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui'
+// import ElementUI from 'element-ui'
+import { Button, Message, Form, FormItem, Input } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 import http from '@/config/http'
@@ -11,7 +12,11 @@ import http from '@/config/http'
 import './assets/style.scss'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(Button)
+Vue.use(Message)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
 Vue.prototype.$axios = http
 
 console.log(`现在处于 ${process.env.NODE_ENV} 环境`)
