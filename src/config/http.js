d7f0e974b2e077d10871ff2804aeaca24c410116
徐忠炜 <943935598@qq.com>
2020-07-09 14:02:10 +0800

    fix：取消全局loading

--- src/config/http.js
+++ src/config/http.js
@@ -1,36 +1,5 @@
 import axios from 'axios'
-import { Message, Loading } from 'element-ui'
-import router from '../router/index'
-
-let loading
-
-function startLoading () {
-  loading = Loading.service({
-    lock: true,
-    text: '加载中...',
-    background: 'rgba(0, 0, 0, .7)'
-  })
-}
-
-function endLoading () {
-  loading.close()
-}
-
-let needLoadingRequestCount = 0
-export function showFullScreenLoading () {
-  if (needLoadingRequestCount === 0) {
-    startLoading()
-  }
-  needLoadingRequestCount++
-}
-
-export function tryHideFullScreenLoading () {
-  if (needLoadingRequestCount <= 0) return
-  needLoadingRequestCount--
-  if (needLoadingRequestCount === 0) {
-    endLoading()
-  }
-}
+import { Message } from 'element-ui'
 
 axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? '//www.xzw999.com/music' : '//localhost:3000'
 
@@ -38,37 +7,30 @@ axios.defaults.withCredentials = true
 
 axios.defaults.timeout = 5000
 
+// 请求拦截器
 axios.interceptors.request.use(config => {
   if (/get/i.test(config.method)) {
     config.params = config.params || {}
     config.params.t = Date.parse(new Date()) / 1000 // 添加时间戳
   }
-  // add cookie
-  showFullScreenLoading()
+  console.log(`正在请求 ${config.url}`)
   return config
 }, err => {
-  console.log(err)
-  Message.error(err.message)
+  Message.error(err)
   return Promise.reject(err)
 })
 
+// 响应拦截器
 axios.interceptors.response.use(res => {
-  tryHideFullScreenLoading()
   return res.data
 }, err => {
+  console.log(`${err.response.config.url} 请求失败`)
+  console.log(err.response)
   switch (err.response.status) {
-    case 301:
-      tryHideFullScreenLoading()
-      Message.error('该功能需要登录才能使用！')
-      router.push('/login')
-      break
     case 404:
-      tryHideFullScreenLoading()
       Message.error(err.response.data.message)
       break
   }
-  tryHideFullScreenLoading()
-  // Message.error(err.message)
   return Promise.reject(err)
 })
 
