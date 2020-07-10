4acaac296b56c82e8a418c2d997ab6d6ca41d1ba
徐忠炜 <943935598@qq.com>
2020-07-10 09:22:34 +0800

    bug：每次刷新都会弹个空的message

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-// import ElementUI from 'element-ui'
 import { Button, Message, Form, FormItem, Input } from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import App from './App.vue'
 
 import http from '@/config/http'
 
@@ -15,10 +14,11 @@ Message({ duration: 1500 })
 
 Vue.config.productionTip = false
 Vue.use(Button)
-Vue.use(Message)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.component(Message.name, Message)
+
 Vue.prototype.$axios = http
 Vue.prototype.$message = Message
 
