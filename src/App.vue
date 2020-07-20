6db51ca1ac51281d671dd0bee10b2b1d701feba2
徐忠炜 <943935598@qq.com>
2020-07-20 18:06:01 +0800

    监听性能

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,22 @@ export default {
 
   },
   mounted () {
+    window.onload = function () {
+      setTimeout(() => {
+        const t = performance.timing
+        const m = performance.memory
+        console.table({
+          DNS查询耗时: (t.domainLookupEnd - t.domainLookupStart).toFixed(0),
+          TCP链接耗时: (t.connectEnd - t.connectStart).toFixed(0),
+          request请求耗时: (t.responseEnd - t.responseStart).toFixed(0),
+          解析dom树耗时: (t.domComplete - t.domInteractive).toFixed(0),
+          白屏时间: (t.responseStart - t.navigationStart).toFixed(0),
+          domready时间: (t.domContentLoadedEventEnd - t.navigationStart).toFixed(0),
+          onload时间: (t.loadEventEnd - t.navigationStart).toFixed(0),
+          js内存使用占比: m ? (m.usedjsHeapSize / m.totaljsHeapSize * 100).toFixed(2) + '%' : undefined
+        })
+      })
+    }
   },
   methods: {
 
